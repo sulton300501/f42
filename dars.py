7499b60e24801dca234cdf1f90906588ca75b5e9
sulton300501 <148205055+sulton300501@users.noreply.github.com>
2023-10-31 06:31:31 +0500

    Update dars.py

---
+++ dars.py
@@ -0,0 +1,16 @@
+bosh = [2,1]
+x=2
+
+
+
+bosh.sort()
+arr = []
+
+
+for i in range(len(bosh)-2):
+    if bosh[i]==x:
+        son = bosh[i]
+        bosh[i]=bosh[i+1]
+        bosh[i+1]=son
+
+print(bosh)
